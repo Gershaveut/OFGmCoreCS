01115db47fdf9129bed48a94453fd2a69ea67588
Gershaveut <vova1234567890012011@yandex.ru>
2024-02-28 18:10:18 +0300

    Добавлен ConsoleHelper

---
+++ OFGmCoreCS/Util/ConsoleHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+namespace OFGmCoreCS.Util
+{
+    public static class ConsoleHelper
+    {
+        public delegate void SignalHandler(ConsoleSignal consoleSignal);
+
+        [DllImport("Kernel32", EntryPoint = "SetConsoleCtrlHandler")]
+        public static extern bool SetSignalHandler(SignalHandler signalHandler, bool add);
+
+        public enum ConsoleSignal
+        {
+            CtrlC = 0,
+            CtrlBreak = 1,
+            Close = 2,
+            LogOff = 5,
+            Shutdown = 6
+        }
+    }
+}
